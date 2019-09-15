a003ce286d615ec66932247846de78459bfdb768
theIceman <kushagrakumar34@gmail.com>
2019-09-16 02:46:13 +0530

    Just Hpynos and Makemaze remaining

--- stpar_spoj.cpp
+++ stpar_spoj.cpp
@@ -2,34 +2,41 @@
 using namespace std;
 
 int main() {
-    int t,n;
+    int t,n, flag;
     cin>>t;
     while(t--) {
         cin>>n;
-        vector<int> kush, new_order;
-        stack <int> truck;
-        int temp, comp =1;
+        stack <int> truck, order;
+        int temp, size, j = 1;
         for(int i=0; i<n; i++) {
             cin>>temp;
-            kush.push_back(temp);
+            if( temp == j ) {
+                order.push(temp);
+                j++;
+            } else {
+                truck.push(temp);
+            }
         }
 
-        for(int i=0; i<n; i++) {
-            if(kush[i] == comp) {
-                new_order.push_back(kush[i]);
+        int flag = 0, comp = order.top()+1;
+        if(order.size() == n){
+            flag = 0;
+        } else {
+            while(!truck.empty()) {
+                if(truck.top() != comp) {
+                    flag = 1;
+                    break;
+                }
                 comp++;
-                continue;
-            } 
-            truck.push(kush[i]);
-        }
-
-        for(int i=0; i<n; i++) {
-            if
+                truck.pop();
+            }
         }
 
 
-
-
+        if(flag)
+            cout<<"NO\n";
+        else
+            cout<<"YES\n";
 
     }
     return 0;
